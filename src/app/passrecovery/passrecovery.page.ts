262a74a55a64b184cdf851a3a7bb68fdcca26f08
DiInfantas <127537754+DiInfantas@users.noreply.github.com>
2024-01-12 16:33:51 -0300

    Cracion registro, login, y recuperacion

---
+++ src/app/passrecovery/passrecovery.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-passrecovery',
+  templateUrl: './passrecovery.page.html',
+  styleUrls: ['./passrecovery.page.scss'],
+})
+export class PassrecoveryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
