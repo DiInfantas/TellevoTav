cdebfa9d2fa97cbbe6daa45f4bf1d6075f643359
DiInfantas <127537754+DiInfantas@users.noreply.github.com>
2024-01-13 16:32:41 -0300

    arreglos y creacion de alerta registro

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
+
+
 export class RegisterPage implements OnInit {
   alertButtons = ['Action'];
-  constructor() { }
+  constructor(
+    private alertController: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
-  
+  async alertaRegistro() {
+    const alerta = await this.alertController.create({
+      header: 'Usuario creado correctamente',
+      subHeader: 'Se le redireccionará al login',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.router.navigate(['/login']);
+          },
+        },
+      ],
+    });
+
+    await alerta.present();
+  }
 }
+
+
+
+
+
