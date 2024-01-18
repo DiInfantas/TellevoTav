6e2d23d6d81a284c93d58fd220063c33e33771a6
DiInfantas <127537754+DiInfantas@users.noreply.github.com>
2024-01-18 15:12:03 -0300

    Ordenamiento proyecto completo (revisar url, no cambia xd)

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  alertButtons = ['Action'];
+  constructor(
+    private alertController: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  async alertaRegistro() {
+    const alerta = await this.alertController.create({
+      header: 'Usuario creado correctamente',
+      subHeader: 'Se le redireccionará al login',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.router.navigate(['/login']);
+          },
+        },
+      ],
+    });
+
+    await alerta.present();
+  }
 }
